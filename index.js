6e230badbf911e7caf249ff01849384491095dc8
Serge <serge.bayet1975@gmail.com>
2019-11-25 10:09:30 +0100

    fix relative path

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ let readInterface = readline.createInterface({
 let serge = [];
 
 console.log(serge);
-serge[0] = fs.readFileSync('./animation/serge1.asc', 'utf-8');
-serge[1] = fs.readFileSync('./animation/serge2.asc', 'utf-8');
-serge[2] = fs.readFileSync('./animation/serge3.asc', 'utf-8');
-serge[3] = fs.readFileSync('./animation/serge4.asc', 'utf-8');
-serge[4] = fs.readFileSync('./animation/serge5.asc', 'utf-8');
-serge[5] = fs.readFileSync('./animation/serge4.asc', 'utf-8');
-serge[6] = fs.readFileSync('./animation/serge3.asc', 'utf-8');
-serge[7] = fs.readFileSync('./animation/serge2.asc', 'utf-8');
+serge[0] = fs.readFileSync('animation/serge1.asc', 'utf-8');
+serge[1] = fs.readFileSync('animation/serge2.asc', 'utf-8');
+serge[2] = fs.readFileSync('animation/serge3.asc', 'utf-8');
+serge[3] = fs.readFileSync('animation/serge4.asc', 'utf-8');
+serge[4] = fs.readFileSync('animation/serge5.asc', 'utf-8');
+serge[5] = fs.readFileSync('animation/serge4.asc', 'utf-8');
+serge[6] = fs.readFileSync('animation/serge3.asc', 'utf-8');
+serge[7] = fs.readFileSync('animation/serge2.asc', 'utf-8');
 
 //console.log(serge);
 // Text + chalk definitions
